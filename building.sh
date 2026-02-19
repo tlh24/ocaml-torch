c96b4dea4bbd5f3454bbe9e3833ebcc3649bf311
Tim Hanson <sideskate@gmail.com>
2026-02-19 11:16:57 -0800

    now it's an actual runnable build script

--- building.sh
+++ building.sh
@@ -1,17 +1,42 @@
+#!/usr/bin/bash
 # Hopefully-helfpul instructions for building ocaml-torch library locally.
 # Tested February 17 2026 Debian 13 (Trixie) and 14 (Forky)
 
-# download libtorch from the pytorch website.
-wget https://download.pytorch.org/libtorch/cu130/libtorch-shared-with-deps-2.10.0%2Bcu130.zip
-unzip libtorch libtorch-shared-with-deps-2.10.0+cu130.zip
-export LIBTORCH=$HOME/var/libtorch
+LIBTORCH=$HOME/var/libtorch
+export LIBTORCH=$LIBTORCH
+
+if [ ! -d "$LIBTORCH" ]; then
+	echo "Directory $LIBTORCH not found."
+	echo "please edit the script to download or specify its location"
+	exit 1
+	# example:
+	# download libtorch from the pytorch website.
+	cd $HOME/var
+	wget https://download.pytorch.org/libtorch/cu130/libtorch-shared-with-deps-2.10.0%2Bcu130.zip
+	unzip libtorch libtorch-shared-with-deps-2.10.0+cu130.zip
+fi
 
 # if you are using a CUDA dev environment not managed by Debian, make sure g++ can find the headers, e.g:
-export C_INCLUDE_PATH="/usr/local/cuda-13.1/targets/x86_64-linux/include"
-export CPLUS_INCLUDE_PATH="/usr/local/cuda-13.1/targets/x86_64-linux/include"
+# if cuda_runtime_api.h is not on gcc search path, throw an error
+if ! echo "#include <cuda_runtime_api.h>" | gcc -E -xc - >/dev/null 2>&1; then
+	echo "Header 'cuda_runtime_api.h' not found in gcc search path. "
+	exit 1
+	# "example:"
+	export C_INCLUDE_PATH="/usr/local/cuda-13.1/targets/x86_64-linux/include"
+	export CPLUS_INCLUDE_PATH="/usr/local/cuda-13.1/targets/x86_64-linux/include"
+else
+    echo "CUDA header found."
+fi
 
 # opam packages:
 opam install dune stdio core ppx_jane patdiff ctypes-foreign npy cmdliner yaml pyml utop
+
+# clean up the source tree, jic
+rm -rf src/wrapper/*generated*
+rm -rf src/wrapper_refcounted/*generated*
+rm -rf src/bindings/*generated*
+rm -rf src/bindings_refcounted/*generated*
+rm -rf src/gen_bindings/*generated*
 
 dune build src/gen_bindings/gen.exe
 cd _build/default/src/gen_bindings/
@@ -38,4 +63,8 @@ dune build -p torch @install
 dune install
 
 # to test out your new library:
+echo 'to test:'
+echo '#require "torch";;'
+echo 'open Torch;;'
+echo 'let t = Tensor.randn [3] ;;'
 LD_PRELOAD=$LIBTORCH/lib/libtorch.so utop
